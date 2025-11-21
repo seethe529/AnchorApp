71474d075003cc8b672ef30366c41120c70debd1
seethe529 <your-email@example.com>
2025-11-21 16:50:17 -0500

    Build 25 - Fix midnight reset notification scheduling with space character

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "24",
+      buildNumber: "25",
       config: {
         usesNonExemptEncryption: false
       },
