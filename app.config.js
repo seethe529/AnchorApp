5ecfc2a47f64a274187d058edfecf01b2b885cc4
seethe529 <your-email@example.com>
2025-11-22 18:19:33 -0500

    Build 31 - Modern UI test build

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "30",
+      buildNumber: "31",
       config: {
         usesNonExemptEncryption: false
       },
