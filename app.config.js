16587e18f693045984cb82ddef00c9e6a30814de
seethe529 <your-email@example.com>
2025-11-22 18:37:14 -0500

    Build 32 - Fix tab bar overlap

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "31",
+      buildNumber: "32",
       config: {
         usesNonExemptEncryption: false
       },
