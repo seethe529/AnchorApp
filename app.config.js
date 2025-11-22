24f357048e60e2fa47e13902ba1ecdd7690a2abb
seethe529 <your-email@example.com>
2025-11-22 18:49:33 -0500

    Build 33 - Fix AI Support input visibility

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "32",
+      buildNumber: "33",
       config: {
         usesNonExemptEncryption: false
       },
