a4cee40a2d17345d5fc00ec9cd993dc158e63aa4
seethe529 <your-email@example.com>
2025-11-21 19:12:03 -0500

    Build 30 - Hourly date check for notification reset

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "29",
+      buildNumber: "30",
       config: {
         usesNonExemptEncryption: false
       },
