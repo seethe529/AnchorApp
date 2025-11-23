3768716ea67648173061659125bff0d0c33a59fc
seethe529 <your-email@example.com>
2025-11-23 12:36:06 -0500

    Build 46: Fix tab bar safe area for iPhone home indicator

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "45",
+      buildNumber: "46",
       config: {
         usesNonExemptEncryption: false
       },
