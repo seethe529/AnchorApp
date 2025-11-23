424282d56f707431742cffd3780f8de9fb3d4f6c
seethe529 <your-email@example.com>
2025-11-23 10:12:01 -0500

    chore: Bump build number to 41

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "40",
+      buildNumber: "41",
       config: {
         usesNonExemptEncryption: false
       },
