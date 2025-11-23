a2dfa59abb5e6d27c82a61732916f2034bbeef4e
seethe529 <your-email@example.com>
2025-11-22 19:40:29 -0500

    chore: Bump build number to 35 for TestFlight

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "34",
+      buildNumber: "35",
       config: {
         usesNonExemptEncryption: false
       },
