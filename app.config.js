03b3868a30881008a306758b898ad573d17b22b4
seethe529 <your-email@example.com>
2025-11-24 10:10:33 -0500

    Fix iOS status bar flash by setting userInterfaceStyle to automatic

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default {
     version: "1.1.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
+    userInterfaceStyle: "automatic",
     splash: {
       image: "./assets/splash.png",
       resizeMode: "contain",
