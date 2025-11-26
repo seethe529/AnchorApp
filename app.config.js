a78ba13d9752f7ca3ba19b268a44d45799abf8c3
seethe529 <your-email@example.com>
2025-11-26 15:11:02 -0500

    Bump to build 54

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "53",
+      buildNumber: "54",
       config: {
         usesNonExemptEncryption: false
       },
