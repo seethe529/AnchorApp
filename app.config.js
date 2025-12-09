c8f7d3c14e2c3474e5c17cb471afc73beb8e979f
seethe529 <your-email@example.com>
2025-12-09 13:08:45 -0500

    Build 61: Update iOS buildNumber

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "60",
+      buildNumber: "61",
       config: {
         usesNonExemptEncryption: false
       },
