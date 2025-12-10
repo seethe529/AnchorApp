80ec3d84b8f4d4541941a3333bd77aca49cee170
seethe529 <your-email@example.com>
2025-12-10 11:01:04 -0500

    Build 63: Fix iOS notification rescheduling - add AppState listener

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "62",
+      buildNumber: "63",
       config: {
         usesNonExemptEncryption: false
       },
