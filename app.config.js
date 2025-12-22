ed03189965bb1b8623d4a31e6e831d8cb368ab84
seethe529 <your-email@example.com>
2025-12-22 12:12:38 -0500

    Bump iOS to v1.2.4 Build 71

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: "Anchor - PTSD Support",
     slug: "anchor-ptsd-support",
     owner: "ryan-charles-lingo",
-    version: "1.2.3",
+    version: "1.2.4",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "70",
+      buildNumber: "71",
       config: {
         usesNonExemptEncryption: false
       },
