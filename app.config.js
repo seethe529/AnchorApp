4a1df0380f5bea364feb877522fb65e6bbc88458
seethe529 <your-email@example.com>
2025-12-22 12:28:47 -0500

    Bump iOS to Build 72

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "71",
+      buildNumber: "72",
       config: {
         usesNonExemptEncryption: false
       },
