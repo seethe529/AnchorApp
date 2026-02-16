b874791d0dc5cf4294261128d0fe66a0356099ec
seethe529 <your-email@example.com>
2026-02-16 09:52:47 -0500

    Increment iOS to Build 93

--- app.config.js
+++ app.config.js
@@ -22,7 +22,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.anchor.ptsd-support",
-      buildNumber: "92",
+      buildNumber: "93",
       config: {
         usesNonExemptEncryption: false
       },
